3659b0617f7e29a14abcd5293f21da0cbcf3134d
Fernando <castro.fernando@me.com>
2020-04-13 21:47:49 -0300

    feature: transaction and balance completed

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,21 @@
 import { Router } from 'express';
-
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import Transaction from '../models/Transaction';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+    const report = {
+      transactions,
+      balance,
+    };
+    return response.json(report);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +23,17 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+    const transaction = new Transaction({
+      title,
+      type,
+      value,
+    });
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+    createTransaction.execute(transaction);
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
